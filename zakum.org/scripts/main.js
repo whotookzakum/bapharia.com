b0bece6ce58f2f836005cfb0308cc461ee9855d5
ACE\ace <mirveo@gmail.com>
2021-06-04 09:02:24 -0700

    Renamed project, working trivia page (needs GUI overhaul but functionality is there). Need Fusion and Trivia page icons

---
+++ zakum.org/scripts/main.js
@@ -0,0 +1,498 @@
+jQuery(document).ready(function($){
+
+    // MENU BUTTONS 
+
+    
+    // Background image change on menu button click
+    // REWRITE TO BE THE FULL PAGE CHANGER
+    $("img.navicon").click(function() {
+        // There's probably a cleaner way to do this like:
+        // 1.   if (this != .classButton) { bg-default opacity 1 }
+        //      if (this = .classButton) { bg-default opacity 0 }
+        // 2.   if (buttonClicked = currentBg) { don't fade } else { fade }
+        //      (way too many conditionals to find buttonClicked and currentBg)
+        // But this works for now
+        var bg = $("#bg-default");
+        var bgaf = $("#bg-af");
+        var bgts = $("#bg-ts");
+        var bgba = $("#bg-ba");
+        var bgsc = $("#bg-sc");
+        var bghs = $("#bg-hs");
+        
+        // Remove the initial fade in effect for bg-default
+        bg.removeClass("bg-default-animation");
+        
+        // Fade out current bg
+        bgaf.stop().animate({opacity: '0'});
+        bgts.stop().animate({opacity: '0'});
+        bgba.stop().animate({opacity: '0'});
+        bgsc.stop().animate({opacity: '0'});
+        bghs.stop().animate({opacity: '0'});
+        bg.stop().animate({opacity: '0'});
+        
+        // Fade in selected page bg
+        if ($(this).is("#buttonTwin")){
+            bgts.stop().animate({opacity: '1'});
+        } 
+        else if ($(this).is("#buttonAegis")) {
+            bgaf.stop().animate({opacity: '1'});
+        }
+        else if ($(this).is("#buttonBlast")) {
+            bgba.stop().animate({opacity: '1'});    
+        }
+        else if ($(this).is("#buttonSpell")) {
+            bgsc.stop().animate({opacity: '1'});
+        }
+        else if ($(this).is("#buttonHeavy")) {
+            bghs.stop().animate({opacity: '1'});
+        }
+        else {
+            if (bg.css("background-image") != "url(images/0.png)") {
+                bg.stop().animate({opacity: '1'});
+                
+                if ($(this).is("#buttonGuide")) {
+                    openTrivia();
+                }
+                
+                
+            }
+        }
+        
+        /* Alternative way to get the element you clicked on
+            $("img.navicon").click(function(event) {
+                var id = event.target.id;
+                console.log('id = ' + id);
+        
+                if (id === "buttonAegis") {
+                $("#background").css("background-image", "url(images/af.png)");
+                }
+            });
+        */
+    });
+    
+
+    // TRIVIA
+    // Generate map names and skill names from a list
+    // Automatically generate "what class's skill is __?" and " __ is an ability for what skill? questions
+    
+
+    var triviaHolder = [
+        { // 1
+            category: "Technical",
+            question: "What is the general name of the company that publishes Blue Protocol?",
+            choice: ["Square ENIX", "Bandai Namco", "Sega", "EA"],
+            answer: ["Bandai Namco"]
+        },
+        { // 2
+            category: "Technical",
+            question: "What is the name of the development team responsible for Blue Protocol?",
+            choice: ["Project Blue", "Project Blue Sky" , "Bandai Namco","Project Sky Blue"],
+            answer: ["Project Sky Blue"]
+        },
+        { // 3
+            category: "Technical",
+            question: "Who is the executive producer of Blue Protocol?",
+            choice: ["Soukichi Shimooka" ,"Keisuke Fukuzaki","Takahiro Suzuki", "Kazuto Kirigaya"],
+            answer: ["Takahiro Suzuki"]
+        },
+        { // 4
+            category: "Class",
+            question: "What was the 5th class added to the game?",
+            choice: ["Aegis Fighter", "Twin Striker", "Heavy Smasher", "Spell Caster"],
+            answer: ["Heavy Smasher"]
+        },
+        { // 5
+            category: "Lore",
+            question: "What is the name of the Aegis Fighter class instructor?",
+            choice: ["Lily", "Korz", "Manuel", "Seila"],
+            answer: ["Korz"]
+        },
+        { // 6
+            category: "Lore",
+            question: "What is the name of the Twin Striker class instructor?",
+            choice: ["Manuel", "Lily", "Seila", "Korz"],
+            answer: ["Manuel"]
+        },
+        { // 7
+            category: "Lore",
+            question: "What is the name of the Blast Archer class instructor?",
+            choice: ["Korz", "Lily", "Manuel", "Seila"],
+            answer: ["Lily"]
+        },
+        { // 8
+            category: "Lore",
+            question: "What is the name of the Spell Caster class instructor?",
+            choice: ["Korz", "Manuel", "Seila", "Lily"],
+            answer: ["Seila"]
+        },
+        { // 9
+            category: "Class",
+            question: "Perfection is an ability for which Twin Striker skill?",
+            choice: ["Blood Axe", "Storm Rush", "Burning Rush", "Vortex Impact"],
+            answer: ["Burning Rush"]
+        },
+        { // 10
+            category: "Class",
+            question: "What class uses the skill War Cry?",
+            choice: ["Aegis Fighter", "Twin Striker", "Blast Archer", "Spell Caster"],
+            answer: ["Twin Striker"]
+        },
+        { // 11
+            category: "Class",
+            question: "What class uses the skill Negative Resonance?",
+            choice: ["Blast Archer", "Spell Caster", "Aegis Fighter", "Twin Striker"],
+            answer: ["Blast Archer"]
+        },
+        { // 12
+            category: "Class",
+            question: "Acceleration is an ability for which Blast Archer skill?",
+            choice: ["Charge Arrow", "Riesel Shower", "Hunter Spirit", "Arrow Rush"],
+            answer: ["Riesel Shower"]
+        },
+        { // 13
+            category: "Class",
+            question: "Blast Archer’s backward + regular attack 1 key results in what?",
+            choice: ["Healing Boost Buff", "Movement Speed Buff", "Attack Buff", "Attack Speed Buff"],
+            answer: ["Movement Speed Buff"]
+        },
+        { // 14
+            category: "Class",
+            question: "Which Spell Caster ability has the same name for two skills?",
+            choice: ["Escalation","Element Escalate", "EP Save", "Rapid Charge"],
+            answer: ["Escalation"]
+        },
+        { // 15
+            category: "Class",
+            question: "Which class uses the skill Engram Charge?",
+            choice: ["Aegis Fighter", "Spell Caster", "Twin Striker", "Blast Archer"],
+            answer: ["Spell Caster"]
+        },
+        { // 16
+            category: "Class",
+            question: "Starlight is an ability for which Aegis Fighter skill?",
+            choice: ["Regeneration","Sunrise Charge","Judgement Shield","Celestial Pillar"],
+            answer: ["Celestial Pillar"]
+        },
+        { // 17
+            category: "Story",
+            question: "Which story character do you meet after defeating an Avalitia for the first time?",
+            choice: ["Jake", "Einrain", "Melrophe", "Aerinse"],
+            answer: ["Melrophe"]
+        },
+        { // 18
+            category: "Story",
+            question: "What is the name of the NPC that gives you a mount imajinn?",
+            choice: ["Roger","Dean","Stadst","Quinn"],
+            answer: ["Stadst"]
+        },
+        { // 19
+            category: "Story",
+            question: "What is the name of the inn that you wake up in after meeting Feste for the first time?",
+            choice: ["The Double-Faced Coin", "Black Masked Inn", "Hunter's Breeze", "The Highland Inn"],
+            answer: ["The Double-Faced Coin"]
+        },
+        { // 20
+            category: "Story",
+            question: "Who is the self-proclaimed “best of the best” adventurer?",
+            choice: ["Einrain", "Jake", "Main Character", "Arinse"],
+            answer: ["Jake"]
+        },
+        { // 21
+            category: "Story",
+            question: "What is the name of the mysterious girl at the Bapharia Shrine?",
+            choice: ["Aylin", "Sophia", "Einrain", "Aerinse"],
+            answer: ["Einrain"]
+        },
+        { // 22
+            category: "Story",
+            question: "What are you, the player, referred to as by some characters in the story?",
+            choice: ["Adventurer","Visitor", "Traveler", "Leaper"],
+            answer: ["Visitor"]
+        },
+        { // 23
+            category: "Story",
+            question: "What is Aenrinse’s bodyguard’s name?",
+            choice: ["Jake", "Einrain", "Feste", "Tyris"],
+            answer: ["Tyris"]
+        },
+        { // 24
+            category: "Story",
+            question: "What is the name of the NPC at Minsterhorn who tells you to go to the Pillar of Divinity?",
+            choice: ["Jake", "Millnae", "Melrophe", "Feste"],
+            answer: ["Millnae"]
+        },
+        { // 25
+            category: "Story",
+            question: "What is the name of the girl in charge of Adventurer Registration at the Frontiering Station?",
+            choice: ["Millnae", "Myulie", "Melroph", "Einrain"],
+            answer: ["Myulie"]
+        },
+        { // 26
+            category: "Lore",
+            question: "What’s the name of the  town that you start in?",
+            choice: ["Bapharia ", "Asterleeds", "Salamzart", "Lapenta"],
+            answer: ["Asterleeds"]
+        },
+        { // 27
+            category: "Lore",
+            question: "What is the desert-themed town called?",
+            choice: ["Ul'dah", "Salamzart", "Death Valley", "Durango"],
+            answer: ["Salamzart"]
+        },
+        { // 28
+            category: "Gameplay",
+            question: "Which color liquid memory increases enemy drop rate?",
+            choice: ["Yellow" , "Red" ,  "Purple" , "Green"],
+            answer: ["Purple"]
+        },
+        { // 29
+            category: "Gameplay",
+            question: "What is the max amount of attachment slots a weapon can have?",
+            choice: ["3", "6", "4" , "5"],
+            answer: ["4"]
+        },
+        { // 30
+            category: "Gameplay",
+            question: "Which element has “high damage explosion” as a burst effect?",
+            choice: ["Light","Dark","Ice","Earth"],
+            answer: ["Light"]
+        },
+        { // 31
+            category: "Gameplay",
+            question: "How many waves does Arena [S Rank] have?",
+            choice: ["30", "100", "25", "70"],
+            answer: ["25"]
+        },
+        { // 32
+            category: "Gameplay",
+            question: "What is the name for the highest level dungeon difficulty?",
+            choice:  ["Final Survey", "Ultra Survey", "Additional Survey", "Advanced Survey"],
+            answer: ["Advanced Survey"]
+        },
+        { // 33
+            category: "Gameplay",
+            question: "What is the name of the boss in Mechanical Ruin Valley?",
+            choice: ["Quad Arm", "Reanimated Terror", "Ancient Android", "Demon Bhor"],
+            answer: ["Quad Arm"]
+        },
+        { // 34
+            category: "Gameplay",
+            question: "What zone is the town Larpal in?",
+            choice: ["Asterleeds", "Fiel Pond", "Andra Basin", "Divine Haven Hill"],
+            answer: ["Divine Haven Hill"]
+        },
+        { // 35
+            category: "Gameplay",
+            question: "What is the first zone outside of Asterleeds called?",
+            choice: ["Divine Haven Hill", "Skyquake Fields", "Andra Basin", "Soundless Foothills"],
+            answer: ["Skyquake Fields"]
+        },
+        { // 36
+            category: "Gameplay",
+            question: "What zone is the dungeon Dragonclaw Valley [Free Exploration] found in?",
+            choice: ["Salamzart", "Minster Hills", "Skyquake fields", "Fiel Pond"],
+            answer: ["Minster Hills"]
+        },
+        { // 37
+            category: "Lore",
+            question: "What is the world of BLUE PROTOCOL called?",
+            choice: ["Asteria", "Bahamar", "Regnus", "Prectus"],
+            answer: ["Regnus"]
+        },
+        { // 38
+            category: "Gameplay",
+            question: "How many warp portals are in Asterleeds?",
+            choice: ["6", "3", "5", "7"],
+            answer: ["7"]
+        },
+        { // 39
+            category: "Class",
+            question: "Which class has the highest base attack on their weapons?",
+            choice: ["Heavy Smasher", "Aegis Fighter", "Spell Caster", "Twin Striker"],
+            answer: ["Twin Striker"]
+        },
+        { // 40
+            category: "Gameplay",
+            question: "What is the default amount of stamina?",
+            choice: ["200", "100", "150", "70"],
+            answer: ["100"]
+        },
+        { // 41
+            category: "Gameplay",
+            question: "How often do most elite monsters spawn?",
+            choice: ["15 minutes", "10 minutes", "7 minutes", "30 minutes"],
+            answer: ["15 minutes"]
+        },
+        { // 42
+            category: "Gameplay",
+            question: "What is the name of the level 18 raid boss?",
+            choice: ["Fiel", "Demidragon", "Undead Prince", "Hydra"],
+            answer: ["Demidragon"]
+        },
+        { // 43
+            category: "Gameplay",
+            question: "What is the max party size?",
+            choice: ["3", "5", "6", "4"],
+            answer: ["6"]
+        },
+        { // 44
+            category: "Gameplay",
+            question: "How many players can be in a raid?",
+            choice: ["15","30","40","20"],
+            answer: ["20"]
+        },
+        { // 45
+            category: "Class",
+            question: "Which class can equip the weapon Bapharia's Guidance?",
+            choice: ["Blast Archer", "Aegis Fighter", "Spell caster", "Twin Striker"],
+            answer: ["Aegis Fighter"]
+        },
+        { // 46
+            category: "Class",
+            question: "Which class has no heal skills?",
+            choice: ["Twin Striker", "Spell Caster", "Aegis Fighter", "Blast Archer"],
+            answer: ["Spell Caster"]
+        },
+        { // 47
+            category: "Gameplay",
+            question: "What is the strongest battle imajinn called?",
+            choice: ["Sealed Tyranny", "Eternal Rage", "Red Splash", "Iron Fangs"],
+            answer: ["Sealed Tyranny"]
+        },
+        { // 48
+            category: "Gameplay",
+            question: "How many healer-type battle imajinn are there?",
+            choice: ["4", "2", "8", "3"],
+            answer: ["3"]
+        },
+        { // 49
+            category: "Technical",
+            question: "What year did the development of BLUE PROTOCOL begin?",
+            choice: ["2014", "2016", "2020", "2021"],
+            answer: ["2016"]
+        },
+        { // 50
+            category: "Gameplay",
+            question: "How many character voice options can you choose from during character creation?",
+            choice: ["7", "1", "5", "3"],
+            answer: ["3"]
+        },
+        { // 51
+            category: "Class",
+            question: "Which class is often considered the biggest support role?",
+            choice: ["Spell Caster", "Blast Archer", "Twin Striker", "Aegis fighter"],
+            answer: ["Blast Archer"]
+        },
+        { // 52
+            category: "Gameplay",
+            question: "How many Permanent Skills & Tactical Abilities are the same for each class?",
+            choice: ["17", "13", "14", "15"],
+            answer: ["14"]
+        },
+        { // 53
+            category: "Gameplay",
+            question: "How many different inner imajinn slots are there?",
+            choice: ["5", "3", "6", "8"],
+            answer: ["5"]
+        },
+        { // 54
+            category: "Gameplay",
+            question: "How many accessory slots are confirmed by the developers?",
+            choice: ["5", "8", "7", "10"],
+            answer: ["7"]
+        },
+        { // 55
+            category: "Gameplay",
+            question: "How much stamina does evasion consume?",
+            choice: ["20", "25", "15", "30"],
+            answer: ["25"]
+        },
+    ]
+    
+    
+    
+    function openTrivia() {
+        var triviaQuestion = $(".triviaQuestion");
+        var triviaCategory = $(".triviaCategory");
+        var triviaChoices = $(".triviaChoices");
+        var qIndex = 0;
+        
+        // Display containers
+        $("#contentViewer").css("display","block");
+        $("#pageTrivia").css("display","inline-block");
+        
+        
+        // Durstenfeld Shuffle
+        function shuffle(array) {
+            for (var i = array.length - 1; i > 0; i--) {
+                var j = Math.floor(Math.random() * (i + 1));
+                var temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        
+        // Shuffle questions
+        shuffle(triviaHolder);
+        
+        function generateQuestion() {
+            
+            // If finished, shuffle deck and restart
+            if (qIndex >= triviaHolder.length) {
+                shuffle(triviaHolder);
+                qIndex = 0;
+            }
+            
+            // Send category & question
+            triviaCategory.html(triviaHolder[qIndex].category);
+            triviaQuestion.html(triviaHolder[qIndex].question);
+
+            // Shuffle choices
+            shuffle(triviaHolder[qIndex].choice);
+
+            // Sends each choice once & resets color
+            triviaChoices.each(function(index) {
+                $(this).css("background-color", "#c8c8c8");
+                $(this).html(triviaHolder[qIndex].choice[(index)]);
+            });
+                
+                
+        }
+        
+        generateQuestion();
+        
+        // Check Answer
+        triviaChoices.click(function (){
+            var myAns = $(this);
+            var correctAns = triviaHolder[qIndex].answer[0];
+
+            // Search choices for correct answer and turn it green; 
+            triviaChoices.each(function() {
+                if ($(this).text() == correctAns) {
+                    $(this).css("background-color", "green");
+                }
+            });
+
+            // If my answer is wrong turn it red
+            if (myAns.text() != correctAns) {
+                myAns.css("background-color", "red");
+            }
+
+            // Go to next question
+            window.setTimeout(generateQuestion, 2000);
+            qIndex++;
+
+        });
+        
+        
+        
+    } // End Trivia
+    
+    
+    
+    // MAP
+
+
+    // SKILL BUILDER
+    
+    });
