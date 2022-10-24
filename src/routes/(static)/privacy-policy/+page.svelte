24287fc5e2d99552f2a8e3848b22dcd9c3991f35
Zakum <mirveo@gmail.com>
2022-10-24 09:57:15 -0700

    privacy policy and error pages

---
+++ src/routes/(static)/privacy-policy/+page.svelte
@@ -0,0 +1,7 @@
+<script>
+	import HeroBanner from "../../../lib/components/HeroBanner.svelte";
+</script>
+
+<HeroBanner title='Privacy Policy' subtext='' />
+
+<h3>Coming soon.</h3>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,26 @@
+<script>
+	import Navbar from '../lib/components/Navbar.svelte';
+	import PageFooter from '../lib/components/PageFooter.svelte';
+	import HeroBanner from '../lib/components/HeroBanner.svelte';
+	import '../lib/styles/globals.scss';
+	import { page } from '$app/stores';
+</script>
+
+<Navbar />
+<main>
+	<HeroBanner title={$page.status} subtext="The requested page could not be found." />
+	<p>
+		Check to make sure the URL is correct. 
+        If you think this is a mistake, let us know on <a href="https://discord.gg/ZKwhn3a4yH" target="_blank" rel="noopener noreferrer">Discord</a>.
+	</p>
+</main>
+<PageFooter />
+
+<style>
+	main {
+		max-width: 1400px;
+		margin: 0 auto;
+		padding: 0 1em;
+		min-height: 60vh;
+	}
+</style>
