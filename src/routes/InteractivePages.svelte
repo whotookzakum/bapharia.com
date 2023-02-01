682cd6ab1eb9c5f9ee89449b9c6a0ac7caa9afc7
Zakum <mirveo@gmail.com>
2023-02-01 01:31:33 -0800

    interactive pages coming soon

--- src/routes/InteractivePages.svelte
+++ src/routes/InteractivePages.svelte
@@ -9,31 +9,51 @@
 			title: "Skills",
 			href: "/skills",
 			iconSrc: "imajinn.svg",
+			disabled: true,
 		},
 		{
 			title: "Crafting",
 			href: "/crafting",
 			iconSrc: "crafting.svg",
+			disabled: true,
 		},
 		{
 			title: "Menu",
 			href: "/menu",
 			iconSrc: "liquidmemory.svg",
+			disabled: true,
 		},
 	];
 </script>
 
 <nav>
 	{#each interactivePages as page}
-		<a class="box" href={page.href}>
-			<img
-				src={`/images/${page.iconSrc}`}
-				alt="Icon"
-				width="48"
-				height="48"
-			/>
-			<h3>{page.title}</h3>
-		</a>
+		{#if page.disabled}
+			<button disabled class="link box">
+				<div class="contents">
+					<img
+						src={`/images/${page.iconSrc}`}
+						alt="Icon"
+						width="48"
+						height="48"
+					/>
+					<h3>{page.title}</h3>
+				</div>
+				<div class="soon">
+					Coming<br />Soon
+				</div>
+			</button>
+		{:else}
+			<a class="link box" href={page.href}>
+				<img
+					src={`/images/${page.iconSrc}`}
+					alt="Icon"
+					width="48"
+					height="48"
+				/>
+				<h3>{page.title}</h3>
+			</a>
+		{/if}
 	{/each}
 </nav>
 
@@ -48,7 +68,7 @@
 		gap: var(--space-s);
 	}
 
-	a {
+	.link {
 		display: grid;
 		place-items: center;
 		gap: var(--space-2xs);
@@ -69,7 +89,7 @@
 		}
 	}
 
-	a:hover {
+	.link:not(:disabled):hover {
 		color: var(--text1);
 		background: var(--surface2);
 		transform: translateY(-4px); // or scale(1.05)
@@ -77,6 +97,37 @@
 
 		img {
 			opacity: 0.7;
+		}
+	}
+
+	button:disabled {
+		background: rgb(22, 22, 22);
+		border-color: rgb(31, 31, 31);
+		position: relative;
+
+		.contents {
+			color: gray;
+		}
+
+		.soon {
+			position: absolute;
+			z-index: 3;
+			bottom: 0;
+			right: 0;
+			margin: 0.7rem 0.5rem;
+			transform: rotate(-40deg);
+			color: var(--accent);
+			font-weight: 500;
+		}
+
+		&::before {
+			content: "";
+			position: absolute;
+			z-index: 2;
+			inset: 0;
+			background: var(--surface2);
+			border: 1px solid var(--surface3);
+			transform: translate(50%, 50%) rotate(-40deg);
 		}
 	}
 </style>
