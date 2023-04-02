e037fed7a608435653b9f7b91d9c71c5d1361f63
Zakum <mirveo@gmail.com>
2023-04-02 04:26:20 -0700

    imagine and weapon database

---
+++ src/routes/db/weapons/+page.js
@@ -0,0 +1,136 @@
+import texts from "$lib/apiext/texts/ja_JP.json";
+import weaponsData from "$lib/apiext/weapons.json";
+import weaponsStatsData from "$lib/apiext/weapon_status_levels.json";
+import recipesData from "$lib/apiext/craft.json";
+import itemsData from "$lib/apiext/items.json";
+import weaponPerks from "$lib/apiext/weaponperks.json";
+import masterPerkPicks from "$lib/apiext/master_weapon_perk_picks.json";
+import perksData from "$lib/apiext/perks.json";
+import lotteryData from "$lib/apiext/master_reward_lottery_groups.json";
+import treasuresData from "$lib/apiext/treasures.json";
+import enemiesData from "$lib/apiext/enemyparams.json";
+
+function getText(ns, id) {
+    const namespaceTexts = texts.find(namespace => namespace.name === ns) || {}
+    const textObj = namespaceTexts.texts.find(obj => obj.id === id) || {}
+    return textObj.text
+}
+
+function findObjectByItemId(objectsArray, innerObject, itemId, keyToFind) {
+    for (let i = 0; i < objectsArray.length; i++) {
+        const innerObjArray = objectsArray[i][innerObject];
+        for (let j = 0; j < innerObjArray.length; j++) {
+            if (innerObjArray[j][keyToFind] === itemId) {
+                return objectsArray[i];
+            }
+        }
+    }
+    return null; // Return null if the object is not found
+}
+
+export const load = async () => {
+
+    const weapons = weaponsData.map(weapon => {
+        weapon.name = getText("weapon_text", weapon.name)
+        const classImg =
+            weapon.equip_class > 10
+                ? `/UI/Icon/Class/UI_IconClass_${weapon.equip_class}.png`
+                : `/UI/Icon/Class/UI_IconClass_0${weapon.equip_class}.png`
+
+
+        const stats = weaponsStatsData.filter(statData => statData.pattern_id == weapon.id)
+
+        let elementImg;
+        switch (weapon.attribute) {
+            case 3:
+                elementImg = `/UI/Icon/Attribute/UI_IconAttribute_1.png`
+                break;
+            case 5:
+                elementImg = `/UI/Icon/Attribute/UI_IconAttribute_2.png`
+                break;
+            case 4:
+                elementImg = `/UI/Icon/Attribute/UI_IconAttribute_3.png`
+                break;
+            case 2:
+                elementImg = `/UI/Icon/Attribute/UI_IconAttribute_4.png`
+                break;
+            case 6:
+                elementImg = `/UI/Icon/Attribute/UI_IconAttribute_5.png`
+                break;
+            case 7:
+                elementImg = `/UI/Icon/Attribute/UI_IconAttribute_6.png`
+                break;
+            default:
+                elementImg = `/UI/Icon/Attribute/UI_IconAttribute_Empty.png`
+        }
+
+        const recipe = recipesData.find(rec => rec.out_item_id === weapon.id) || null
+        if (recipe) {
+            recipe.materials = recipe.materials.map(mat => {
+                const itemData = itemsData.find(item => item.id === mat.item_id)
+                return {
+                    ...mat,
+                    name: getText("item_text", itemData.name),
+                    source: getText("item_text", itemData.obtaining_route_detail_id)
+                }
+            })
+        }
+
+        const abilities = masterPerkPicks.filter(perk => perk.table_id === weapon.level_table).map(currentPerk => {
+            const weaponPerk = weaponPerks.find(perk => perk.id === currentPerk.perk_id)
+            let perkData = perksData.find(perk => perk.id === weaponPerk.perk_id)
+            const name = getText("perk_text", perkData.ability_name1)
+
+            return {
+                ...currentPerk,
+                probability: currentPerk.weight,
+                name
+            }
+        })
+
+        // Assuming no duplicates of the Weapon in this specific Treasure Box... findObjectByItemId() only returns the first instance.
+        // Since it looks like treasure boxes have, for example, 2 lotteries in them at 50/50 and both of them contain the weapon, we can assume 100% chance of getting a lottery with a weapon. 
+        // Enemy > Treasure > Lottery > Weapon
+        let treasureSources = []
+
+        // The lottery that contains the weapon
+        const availableLotteries = findObjectByItemId(lotteryData, "rewards", weapon.id, "item_id")
+        if (availableLotteries) {
+            // The treasure box that contains the lottery
+            const treasureData = findObjectByItemId(treasuresData, "lot_rate", availableLotteries.id, "reward_master_id")
+
+            if (treasureData) {
+                // The enemy that drops the treasure box
+                const enemyData = findObjectByItemId(enemiesData, "drop_items", treasureData.id, "item_index")
+
+                if (enemyData) {
+                    // The drop data of the treasure box
+                    const dropData = enemyData.drop_items.find(drop => drop.item_index === treasureData.id)
+                    treasureSources.push(
+                        {
+                            name: getText("enemyparam_text", enemyData.name_id),
+                            probability: dropData.drop_rate / 100,
+                            location: dropData.content_id
+                        }
+                    )
+                }
+            }
+        }
+
+        return {
+            ...weapon,
+            thumb: `/UI/Icon/Weapon/UI_Icon_${weapon.id}.png`,
+            classImg,
+            stats,
+            elementImg,
+            recipe,
+            abilities,
+            treasureSources
+        }
+    })
+    // console.log(weapons)
+
+    return {
+        weapons
+    }
+}
---
+++ src/routes/db/weapons/+page.svelte
@@ -0,0 +1,151 @@
+<script>
+    export let data;
+</script>
+
+<main>
+    <h1>Weapons</h1>
+    {#each data.weapons as weapon}
+        <div class="box surface">
+            <div class="flex" style="gap: 0.5rem">
+                <img src={weapon.thumb} alt="" />
+                <div class="grid">
+                    <h5>{weapon.name}</h5>
+                    <span>{weapon.id}</span>
+                </div>
+            </div>
+            <div>
+                Class: <img
+                    src={weapon.classImg}
+                    alt=""
+                    width="28"
+                    height="28"
+                />
+            </div>
+            <div>Lv. {weapon.weapon_max_level}</div>
+            <div>
+                Sells for {weapon.price_player_sells}
+                <img
+                    src="/UI/Common/UI_CmnMoney1.png"
+                    alt=""
+                    width="24"
+                    height="24"
+                    style="margin: -7px -4px"
+                />
+            </div>
+            {#if weapon.price_player_buys !== 999999999}
+                <div>
+                    Buy for {weapon.price_player_buys}
+                    <img
+                        src="/UI/Common/UI_CmnMoney1.png"
+                        alt=""
+                        width="24"
+                        height="24"
+                        style="margin: -7px -4px"
+                    />
+                </div>
+            {/if}
+            {#if weapon.is_for_weapon_stickers === 0}
+                <div>
+                    Element <img
+                        src={weapon.elementImg}
+                        alt=""
+                        width="24"
+                        height="24"
+                    />
+                    {weapon.stats[weapon.stats.length - 1].attribute_value}
+                </div>
+                <div>
+                    ATK {weapon.stats[weapon.stats.length - 1].offensive_power}
+                </div>
+                <div>STR {weapon.stats[weapon.stats.length - 1].str}</div>
+                <div>VIT {weapon.stats[weapon.stats.length - 1].vit}</div>
+                <div>DEX {weapon.stats[weapon.stats.length - 1].dex}</div>
+                <div>MND {weapon.stats[weapon.stats.length - 1].mnd}</div>
+                <div>INT {weapon.stats[weapon.stats.length - 1].int}</div>
+                <div>
+                    Crit Damage {weapon.stats[weapon.stats.length - 1]
+                        .critical_power}%
+                </div>
+
+                {#if weapon.recipe}
+                    <hr />
+                    <div>
+                        Req. Adventurer rank {weapon.recipe.difficulty} to craft
+                    </div>
+                    {#each weapon.recipe.materials as material}
+                        <div>
+                            {material.name} x{material.need_num} ({material.source})
+                        </div>
+                    {/each}
+                    <div>
+                        LUNO {weapon.recipe.use_money}
+                        <img
+                            src="/UI/Common/UI_CmnMoney1.png"
+                            alt=""
+                            width="24"
+                            height="24"
+                            style="margin: -7px -4px"
+                        />
+                    </div>
+                    <div>Success:</div>
+                    <div>1 socket {weapon.recipe.base_ability_rate1}%</div>
+                    <div>2 socket {weapon.recipe.base_ability_rate2}%</div>
+                    <div>3 socket {weapon.recipe.base_ability_rate3}%</div>
+                    <div>4 socket {weapon.recipe.base_ability_rate4}%</div>
+
+                    <div>Great Success ({weapon.recipe.bonus_rate}%):</div>
+                    <!-- <div>1 socket {weapon.recipe.bonus_ability_rate1}%</div>
+                    <div>2 socket {weapon.recipe.bonus_ability_rate2}%</div>
+                    <div>3 socket {weapon.recipe.bonus_ability_rate3}%</div>
+                    <div>4 socket {weapon.recipe.bonus_ability_rate4}%</div> -->
+                    <div>1 socket 0%</div>
+                    <div>2 socket 15%</div>
+                    <div>3 socket 50%</div>
+                    <div>4 socket 35%</div>
+                    <div>
+                        Great Success ticket cost: {weapon.recipe
+                            .great_success_tokens}
+                    </div>
+                {/if}
+
+                {#if weapon.abilities.length > 0}
+                    <hr />
+                    {#each weapon.abilities as ability}
+                        <div>
+                            {ability.name}
+                            Great Success: +{ability.big_success_min_value}% ~ +{ability.big_success_max_value}%;
+                            Success: +{ability.success_min_value}% ~ +{ability.success_max_value}%;
+                            Probability: {ability.probability}%
+                        </div>
+                    {/each}
+                {/if}
+
+                {#if weapon.treasureSources.length > 0}
+                    <hr />
+                    {#each weapon.treasureSources as source}
+                        <div>
+                            {source.name}
+                            {source.probability}% {source.location}
+                        </div>
+                    {/each}
+                {/if}
+            {/if}
+        </div>
+    {/each}
+</main>
+
+<!-- {#each Object.keys(data) as key}
+    {key}
+{/each} -->
+<style lang="scss">
+    main {
+        margin: auto;
+        max-width: 65ch;
+        display: grid;
+        gap: 0.5rem;
+    }
+
+    h5 {
+        margin: 0;
+    }
+</style>
