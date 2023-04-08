89786da64b40b9eb1e4b2876364c45404f854264
Zakum <mirveo@gmail.com>
2023-04-08 02:24:41 -0700

    costumes db

---
+++ src/routes/db/costumes/+page.js
@@ -0,0 +1,46 @@
+import texts from "$lib/apiext/texts/ja_JP.json";
+import costumesData from "$lib/apiext/costume.json";
+
+function getText(ns, id) {
+    const namespaceTexts = texts.find(namespace => namespace.name === ns) || {}
+    const textObj = namespaceTexts.texts.find(obj => obj.id === id) || {}
+    return textObj.text
+}
+
+function getPartsPath(name) {
+    if (name.includes("hat")) return "Hat"
+    if (name.includes("bdl")) return "Lower"
+    if (name.includes("udw")) return "Underwear"
+    if (name.includes("sho")) return "Shoes"
+    if (name.includes("bdu")) return "Upper"
+    if (name.includes("glv")) return "Glove"
+    if (name.includes("acc_hpb")) return "AccessoryHipBack"
+    if (name.includes("acc_hdt")) return "AccessoryHeadTop"
+    if (name.includes("acc_hdm")) return "AccessoryHeadMid"
+    if (name.includes("acc_hdl")) return "AccessoryHeadLow"
+    if (name.includes("acc_hde")) return "AccessoryHeadEar"
+    if (name.includes("acc_fng")) return "AccessoryFinger"
+    if (name.includes("acc_bdb")) return "AccessoryBodyBack"
+    else return
+}
+
+export const load = async () => {
+
+    const costumes = costumesData.map(costume => {
+        const name = getText("costume_text", costume.name)
+        const desc = getText("costume_text", costume.desc)
+        const pathToThumb = getPartsPath(costume.costume_parts_name)
+        const thumb = pathToThumb ? `/UI/Icon/Costume/${pathToThumb}/${costume.icon_name}.png` : ""
+    
+        return {
+            ...costume,
+            name,
+            desc,
+            thumb
+        }
+    })
+
+    return {
+        costumes
+    }
+}
---
+++ src/routes/db/costumes/+page.svelte
@@ -0,0 +1,40 @@
+<script>
+    export let data;
+</script>
+
+<main>
+    <h1>Costumes</h1>
+    {#each data.costumes as costume}
+        <div class="box surface">
+            <div class="flex" style="gap: 0.5rem">
+                {#if costume.thumb}
+                    <img src={costume.thumb} alt="" style="flex: 0" />
+                {/if}
+                <div class="grid">
+                    <h5>{costume.name}</h5>
+                    <div>{costume.id}</div>
+                </div>
+            </div>
+            <div>{costume.desc}</div>
+        </div>
+    {/each}
+</main>
+
+<style lang="scss">
+    main {
+        margin: auto;
+        max-width: 65ch;
+        display: grid;
+        gap: 0.5rem;
+    }
+
+    h5 {
+        margin: 0;
+    }
+
+    h6 {
+        font-size: inherit;
+        margin-top: 0.5rem;
+        color: var(--accent);
+    }
+</style>
