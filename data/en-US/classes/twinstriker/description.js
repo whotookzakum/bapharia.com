089d6230db86b0855c76045dfc839e7e1d7596c5
Zakum <mirveo@gmail.com>
2023-04-11 14:57:51 -0700

    clear master

--- data/en-US/classes/twinstriker/description.js
+++
@@ -1,8 +0,0 @@
-export const CLASS_DESCRIPTION_TS = {
-    name: "Twin Striker",
-    icon: "images/tsicon.svg",
-    chart: "images/skills/tschart.png",
-    elements: '<img src="images/crafting/fire.png" style="filter: brightness(0.1) sepia(1) saturate(5000%) hue-rotate(0)" draggable="false">',
-    text: '<span style="font-style: italic;">A front line melee class that wields axes in both hands.<br></span><span style="position: relative; top: 0.5vw;"><span style="font-weight: bold;">Combo Meter<br></span>Damaging enemies builds combo meter, which increases damage. Meter will reset to 0 if knocked down.</span>', // berserk
-    video: "images/skills/ts.mp4"
-}
