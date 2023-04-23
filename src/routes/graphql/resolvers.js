be7af211794aeb96e1cb7fd8507c471374bcdd20
Zakum <mirveo@gmail.com>
2023-04-23 02:59:23 -0700

    item icons fixed

--- src/routes/graphql/resolvers.js
+++ src/routes/graphql/resolvers.js
@@ -23,6 +23,7 @@ import lotteryData from "./japan/master_reward_lottery_groups.json";
 import skillsData from "./japan/skill_data.json";
 import mapsData from "./maps.json";
 import adventureBoardsData from "./japan/master_adventure_board.json"
+import itemIcons from "./itemicons.json"
 import _ from "lodash";
 
 
@@ -271,23 +272,24 @@ function getThumbnail(entryType, category, id, class_type) {
     }
 
     if (entryType === "item") {
-        switch (category) {
-            case 0: return `/UI/Icon/Item/Consumption/UI_Icon_${id}.png`
-            case 2: return `/UI/Icon/Item/Material/UI_Icon_Fusion_1.png`
-            case 4: return `/UI/Icon/Item/EnemyMaterial/UI_Icon_Idea.png`
-            case 6: return `/UI/Icon/Item/Consumption/UI_Icon_Itembox_Select.png`
-            case 7: return `/UI/Icon/Item/Consumption/UI_Icon_Itembox_Random.png`
-            case 1:
-            // return `/UI/Icon/Item/Material/UI_Icon_${id}.png`
-            case 3:
-            // ``
-            case 5:
-            // return `/UI/Icon/Item/Consumption/UI_Icon_gashabox_RichSet_${id}.png`
-            // return `/UI/Icon/Item/Consumption/UI_Icon_Itembox_Set.png`
-            // return `/UI/Icon/Item/Consumption/UI_Icon_UI_Icon_gashabox_Gesture.png`
-            // return `/UI/Icon/Item/Consumption/UI_Icon_UI_Icon_gashabox_Stamp.png`
-            default: return `/UI/Icon/Common/UI_Icon_Empty.png`
-        }
+        return itemIcons[id]
+        // switch (category) {
+        //     case 0: return `/UI/Icon/Item/Consumption/UI_Icon_${id}.png`
+        //     case 2: return `/UI/Icon/Item/Material/UI_Icon_Fusion_1.png`
+        //     case 4: return `/UI/Icon/Item/EnemyMaterial/UI_Icon_Idea.png`
+        //     case 6: return `/UI/Icon/Item/Consumption/UI_Icon_Itembox_Select.png`
+        //     case 7: return `/UI/Icon/Item/Consumption/UI_Icon_Itembox_Random.png`
+        //     case 1:
+        //     // return `/UI/Icon/Item/Material/UI_Icon_${id}.png`
+        //     case 3:
+        //     // ``
+        //     case 5:
+        //     // return `/UI/Icon/Item/Consumption/UI_Icon_gashabox_RichSet_${id}.png`
+        //     // return `/UI/Icon/Item/Consumption/UI_Icon_Itembox_Set.png`
+        //     // return `/UI/Icon/Item/Consumption/UI_Icon_UI_Icon_gashabox_Gesture.png`
+        //     // return `/UI/Icon/Item/Consumption/UI_Icon_UI_Icon_gashabox_Stamp.png`
+        //     default: return `/UI/Icon/Common/UI_Icon_Empty.png`
+        // }
     }
 
     if (entryType === "skill") {
