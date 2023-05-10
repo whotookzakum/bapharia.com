688d5c7987516a0db50fc1ce1724059be22f9915
Zakum <mirveo@gmail.com>
2023-05-09 19:24:36 -0700

    scroll tracker

--- src/routes/guides/[slug]/Article.svelte
+++ src/routes/guides/[slug]/Article.svelte
@@ -1,29 +1,7 @@
 <script>
-	import { goto } from "$app/navigation";
-	import { page } from "$app/stores";
 	import MetaTags from "$lib/components/MetaTags.svelte";
-	import { onMount } from "svelte";
 	export let title, author, date, caption, category, bannerImg, thumbImg;
 	const formattedDate = new Date(date).toLocaleDateString();
-
-	// onMount(() => {
-	// 	const anchors = document.querySelectorAll(".toc a");
-	// 	console.log(anchors);
-
-	// 	anchors.forEach((anchor) => {
-	// 		anchor.addEventListener("click", (e) => {
-	// 			const targetId = new URL(anchor.href).hash.replace("#", "");
-	// 			const target = document.getElementById(targetId);
-	// 			console.log(target);
-	// 			e.preventDefault();
-	// 			window.scrollTo({
-	// 				top: target.offsetTop,
-	// 				behavior: "smooth",
-	// 			});
-	// 			window.history.pushState(_, "", "/hello")
-	// 		});
-	// 	});
-	// });
 </script>
 
 <MetaTags title={`${title} — Bapharia`} description={caption} />
