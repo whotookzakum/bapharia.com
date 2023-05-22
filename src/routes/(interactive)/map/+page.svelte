70ba144f0856c845d7750d943e95e1cb18304b8a
Zakum <mirveo@gmail.com>
2023-05-22 08:02:51 -0700

    map marker update url

--- src/routes/(interactive)/map/+page.svelte
+++ src/routes/(interactive)/map/+page.svelte
@@ -1,5 +1,7 @@
 <script>
     // https://github.com/ngyewch/svelte-leaflet/issues/3#issuecomment-1170333938
+    import { page } from "$app/stores";
+
     import "leaflet/dist/leaflet.css";
     import "./leaflet.scss";
     import L from "leaflet?client";
@@ -9,13 +11,15 @@
         Marker,
         Icon,
         Popup,
-        Tooltip,
+        Tooltip
     } from "svelte-leafletjs?client";
     // import LeafletMap from "$lib/components/leaflet/LeafletMap.svelte";
     import { browser } from "$app/environment";
     import MetaTags from "$lib/components/MetaTags.svelte";
     import MapControls from "$lib/components/leaflet/MapControls.svelte";
     import { userLocale, markersVisibility } from "$lib/stores";
+    import { goto } from "$app/navigation";
+    import { onMount } from "svelte";
 
     let leafletMap;
     export let data;
@@ -39,6 +43,64 @@
     const imageOverlayOptions = {
         attribution: "© Bandai Namco Online Inc. © Bandai Namco Studios Inc.",
     };
+
+    
+    
+    // onMount(() => {
+    //     const cachedMarker = localStorage.getItem("map-marker")
+    //     if (cachedMarker) {
+    //         // Make sure marker exists on current zone, i.e. Cty001
+    //         // Will not work if duplicate marker names exist on multiple zones, i.e. LockerStorage1 exists in both Cty001 and Cty002. Leaving off at Cty001 and visitng Cty002 the next time would open Cty002's marker.
+    //         if (zone.markers.some(marker => marker.id === cachedMarker)) {
+    //             $page.url.searchParams.set("marker", cachedMarker)
+    //         }
+    //         // Remove so that it's not confusing why the marker is open next time, i.e. opening a marker on Cty001, viewing Cty002 but not opening a marker, then next visit starting at Cty001 and seeing the marker open.
+    //         else {
+    //             localStorage.clear("map-marker")
+    //         }
+    //     }
+
+    //     const cachedZone = localStorage.getItem("map-zone")
+    //     if (cachedZone) {
+    //         $page.url.searchParams.set("zone", cachedZone)
+    //     } else {
+    //         localStorage.setItem("map-zone", $page.url.searchParams.get("zone") || "Cty001")
+    //     }
+
+    //     goto(`?${$page.url.searchParams.toString()}`, {
+    //         noScroll: true,
+    //         replaceState: true,
+    //         keepFocus: true,
+    //     });
+    // })
+
+    function handlePopupOpen(e) {
+        console.log(leafletMap.getMap())
+        $page.url.searchParams.set("marker", "hi")
+        // localStorage.setItem("map-marker", )
+        goto(`?${$page.url.searchParams.toString()}`, {
+            noScroll: true,
+            replaceState: true,
+            keepFocus: true,
+        });
+    }
+
+    function handlePopupClose() {
+        $page.url.searchParams.delete("marker")
+        // localStorage.clear("map-marker")
+        goto(`?${$page.url.searchParams.toString()}`, {
+            noScroll: true,
+            replaceState: true,
+            keepFocus: true,
+        });
+    }
+
+    let popupz;
+    // $: console.log(popupz?.getPopup())
+    // $: console.log(leafletMap?.getMap()?.openPopup(L.Popup(popupz)));
+
+    
+    // $: leafletMap?.getMap()?.openPopup(popupz);
 </script>
 
 <MetaTags
@@ -60,7 +122,9 @@
     <LeafletMap
         bind:this={leafletMap}
         options={mapOptions}
-        class="leaflet-wrapper"
+        events={["popupopen", "popupclose"]}
+        on:popupopen={handlePopupOpen}
+        on:popupclose={handlePopupClose}
     >
         <ImageOverlay
             imageUrl={zone.imgSrc}
@@ -86,7 +150,8 @@
                             }}
                         />
                     {/if}
-                    <Popup>
+
+                    <Popup bind:this={popupz} >
                         <small style="color: var(--text2)">{marker.id}</small
                         ><br />
                         <strong>{marker.name[$userLocale]}</strong><br />
