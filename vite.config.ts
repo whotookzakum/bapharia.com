568dfcb7e14b6a053ef60e463571ddef55739a7a
Zakum <mirveo@gmail.com>
2023-05-22 17:55:06 -0700

    remove houdini and db

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
-import houdini from 'houdini/vite'
 import { isoImport } from 'vite-plugin-iso-import'
 
 const config: UserConfig = {
-	plugins: [isoImport(), houdini(), sveltekit()]
+	plugins: [isoImport(), sveltekit()]
 };
 
 export default config;
