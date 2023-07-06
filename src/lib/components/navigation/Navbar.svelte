24d9a481b0a860c74c7a28dbab73cd744a338ca1
Zakum <mirveo@gmail.com>
2023-07-05 18:32:00 -0700

    change navbar to top nav

--- src/lib/components/navigation/Navbar.svelte
+++ src/lib/components/navigation/Navbar.svelte
@@ -22,11 +22,8 @@
 
 <!-- https://nicobachner.com/sveltekit-theme-switch -->
 <!-- https://web.dev/building-a-color-scheme -->
-<div
-	class="navbar-wrapper flex"
-	class:expanded={isExpanded}
-	class:collapsed={!isExpanded}
->
+
+<div class="navbar-wrapper">
 	<a
 		href="#main"
 		class="link show-when-focus-visible"
@@ -34,17 +31,8 @@
 	>
 		Skip to main content and focus
 	</a>
-	<nav class="navbar flex" class:mobile-expanded={isMobileExpanded}>
-		<div class="nav-header flex">
-			<a href="/" class="link link-home link-with-icon">
-				<img class="logo" src="/images/logo.png" alt="Logo" />
-				<span class="link-text show-when-expanded" role="text">
-					<span>Bapharia</span>
-					<span class="hidden-text" aria-hidden="true"
-						>{randomMessage}</span
-					>
-				</span>
-			</a>
+	<nav class="navbar flex g-50" class:mobile-expanded={isMobileExpanded}>
+		<!-- <div class="nav-header">
 			<div class="mobile-controls flex g-25">
 				<LocaleSelector isCollapsed={true} />
 				<label class="mobile-drawer-label" for="drawerToggle">
@@ -61,93 +49,67 @@
 					/>
 				</label>
 			</div>
+		</div> -->
+		<a
+			href="/"
+			class="link logo-wrapper styled-link"
+			class:active={$page.url.pathname === "/"}
+			style:padding="0.5rem"
+			
+		>
+			<!-- <span class="logo icon" /> -->
+			<img
+				class="logo grid"
+				src="/images/logo.png"
+				alt="Home"
+				width="38"
+				height="38"
+				style:place-items="center"
+			/>
+		</a>
+		<input type="search" placeholder="Search" name="" id=""/>
+		{#each links as link}
+			<a
+				href={link.href}
+				class="link styled-link hover-link"
+				class:active={$page.url.pathname.includes(link.href)}
+			>
+				{link.name[$userLocale]}
+			</a>
+		{/each}
+
+		<div class="extras">
+			<LocaleSelector isCollapsed={!isExpanded} />
+			<ThemeToggle />
 		</div>
 
-		<div class="nav-body">
-			<hr />
-			<TimeInJapan {isExpanded} />
-			<hr />
-			<div class="links">
-				{#each links as link}
-					<a
-						href={link.href}
-						class="link link-with-icon"
-						class:active={$page.url.pathname.includes(link.href)}
-						on:click={() => (isMobileExpanded = !isMobileExpanded)}
-					>
-						<span
-							class="icon"
-							style:mask-image="url({link.imgSrc})"
-							style="-webkit-mask-image: url({link.imgSrc})"
-						/>
-						<span class="show-when-expanded"
-							>{link.name[$userLocale]}</span
-						>
-					</a>
-				{/each}
-			</div>
-			<hr />
-			<div class="nav-extras">
-				<LocaleSelector isCollapsed={!isExpanded} />
-				<ThemeToggle />
-			</div>
-		</div>
-		<div class="nav-footer">
-			<a
-				class="site-version"
-				href="/changelog"
-				class:active={$page.url.pathname === "/changelog"}
-				>{isExpanded ? "Version 0.9.2" : "v0.9.2"}</a
-			>
-			<label class="nav-expander flex" aria-hidden="true">
-				<Icon
-					icon={isExpanded
-						? "ri:expand-left-line"
-						: "ri:expand-right-line"}
-					width="38"
-					height="38"
-					style="margin-left: auto;"
-				/>
-				<span class="visually-hidden">Expand/Collapse Sidebar</span>
-				<input
-					class="visually-hidden"
-					type="checkbox"
-					bind:checked={isExpanded}
-				/>
-			</label>
-		</div>
+		<!-- <TimeInJapan isExpanded={false} /> -->
 	</nav>
 </div>
 
 <style lang="scss">
+	$bg: #1c1f2e;
+	$surface1: #6315ac;
+	$surface2: #ab1572;
 	.navbar-wrapper {
 		position: sticky;
-		height: 100vh;
+		width: 100%;
 		z-index: 1002;
-		background: var(--surface1);
+		background: rgb(35 39 47/0.9);
+		backdrop-filter: blur(50px);
 		top: 0;
-		padding: 0.5rem 0;
-		overflow-y: auto;
-		flex-direction: column;
+		padding: 0 1rem;
 	}
 
 	.navbar {
-		height: 100%;
-		flex-direction: column;
-		width: var(--nav-width);
 		--icon-size: 38px;
+		max-width: 1400px;
+		width: 100%;
+		margin: auto;
+		align-items: center;
+		height: 62px;
 	}
 
-	:global(.navbar + .contents) {
-		--nav-width: 70px;
-	}
-
-	.expanded,
-	:global(.expanded + .contents) {
-		--nav-width: 250px;
-	}
-
-	.collapsed .show-when-expanded,
 	.show-when-focus-visible:not(:focus-visible) {
 		clip: rect(0 0 0 0);
 		clip-path: inset(50%);
@@ -158,69 +120,34 @@
 		width: 1px;
 	}
 
-	// HEADER ======================================================
+	input {
+		border: none;
+		background: rgba(120,131,155,.2);
+		padding: 0.5rem 1rem;
+		border-radius: 3rem;
+		flex: 1;
+		max-width: 300px;
 
-	.nav-header {
-		align-items: center;
-		justify-content: space-between;
-
-		img.logo {
-			max-width: var(--icon-size);
-			filter: brightness(0.95)
-				drop-shadow(0 2px 1px var(--surface-shadow));
-		}
-
-		.link-text {
-			display: grid;
-			grid-template-rows: auto 0fr;
-		}
-
-		.hidden-text {
-			font-size: var(--step--2);
-			color: var(--accent);
-			overflow: hidden;
-			opacity: 0;
-			transition-delay: 0.05s;
+		&::placeholder {
+			color: inherit;
 		}
 	}
+
+	// HEADER ======================================================
 
 	// LINKS =====================================================
 
-	.collapsed .link {
-		gap: 0;
-	}
-
 	.link {
-		align-items: center;
 		border: none;
-		position: relative;
-		padding: 0.5rem 1rem;
-		max-inline-size: none;
-		display: block;
-		gap: 1rem;
-
-		&:hover,
-		&:focus-visible {
-			color: unset;
-		}
-
-		&.active,
-		&:focus-visible,
-		&:hover {
-			&::after {
-				content: "";
-				position: absolute;
-				inset: 0;
-				background: var(--surface2);
-				z-index: -1;
-			}
-		}
+		padding: 0.75rem;
+		border-radius: 3rem;
+		// background: green;
 	}
 
-	// Currently using .show-when-expanded, but might be possible to show the icon column and hide the text column
-	.link-with-icon {
-		display: grid;
-		grid-template-columns: auto 1fr;
+	.hover-link {
+		&:hover {
+			background: rgba(246,247,249,.05);
+		}
 	}
 
 	.icon {
@@ -228,94 +155,25 @@
 		display: inline-block;
 		height: var(--icon-size);
 		width: var(--icon-size);
-		background-color: var(--text1);
 		mask: no-repeat center / contain;
 		-webkit-mask: no-repeat center / contain;
 	}
 
 	.active {
 		color: var(--accent) !important;
-
-		.icon {
-			background-color: var(--accent) !important;
-		}
 	}
+
 
 	// EXTRAS ======================================================
 
-	.nav-extras {
+	.extras {
 		display: flex;
 		gap: 0.5rem;
 		align-items: center;
-		padding: 0 12px;
-		flex-wrap: wrap;
-	}
-
-	.collapsed .nav-extras {
-		display: grid;
-		justify-content: center;
-		gap: 0.25rem;
+		margin-left: auto;
 	}
 
 	// FOOTER ====================================================
-
-	.nav-footer {
-		display: flex;
-		align-items: flex-end;
-		justify-content: space-between;
-		gap: 1rem;
-		margin-top: auto;
-		padding: 0rem 12px 4px 12px;
-		width: 100%;
-	}
-
-	.collapsed .nav-footer {
-		display: grid;
-		justify-items: center;
-	}
-
-	.site-version {
-		border: none;
-		font-size: var(--step--2);
-		color: var(--text2);
-		text-decoration: underline;
-
-		&:hover,
-		&:focus-visible {
-			color: var(--accent);
-		}
-	}
-
-	.expanded .site-version {
-		margin: 0 4px;
-		padding: 0.6rem 0.2rem;
-	}
-
-	label {
-		width: 44px;
-		height: 44px;
-		display: grid;
-		place-content: center;
-
-		&:hover {
-			border-radius: 5px;
-			background-color: var(--surface2);
-		}
-
-		&:has(:focus-visible) {
-			border-radius: 5px;
-			background-color: var(--surface2);
-			outline: 2px solid var(--accent);
-		}
-	}
-
-	@supports not selector(:has(*)) {
-		label:focus-within {
-			border-radius: 5px;
-			background-color: var(--surface2);
-			outline: 2px solid var(--accent);
-		}
-	}
 
 	// MOBILE ======================================
 
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,17 +1,9 @@
 <script>
-    import '$lib/styles/globals.scss';
+    import "$lib/styles/globals.scss";
     import Navbar from "$lib/components/navigation/Navbar.svelte";
-    import { GoogleAnalytics } from '@beyonk/svelte-google-analytics'
+    import { GoogleAnalytics } from "@beyonk/svelte-google-analytics";
 </script>
 
-<GoogleAnalytics properties={['G-WFYTLGB8B9']} />
-
-<div class="layout">
-	<Navbar />
-	<slot />
-</div>
-
-
-<style lang="scss">
-	
-</style>
+<GoogleAnalytics properties={["G-WFYTLGB8B9"]} />
+<Navbar />
+<slot />
