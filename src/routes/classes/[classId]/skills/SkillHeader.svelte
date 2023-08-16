de5461ba6dad4ea6391f90ab22112876122cc9fc
Zakum <mirveo@gmail.com>
2023-08-15 19:31:18 -0700

    twin strike animation cancels (from memory)

--- src/routes/classes/[classId]/skills/SkillHeader.svelte
+++ src/routes/classes/[classId]/skills/SkillHeader.svelte
@@ -96,7 +96,7 @@
         />
         <div>
             <h3 class="toc-exclude" id={skill.name.en_US.toLowerCase().replace(" ", "-").replace(" ", "-")}>
-                {skill.name[$userLocale]} {skill.id}
+                {skill.name[$userLocale]}
             </h3>
             <div>
                 {#if elementId !== "Empty"}
