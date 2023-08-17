21def7c058a74b8349d7e7f1d99e220b6f32f85e
Zakum <mirveo@gmail.com>
2023-08-17 16:12:40 -0700

    boards page search filters

--- src/routes/boards/+page.svelte
+++ src/routes/boards/+page.svelte
@@ -1,34 +1,145 @@
 <script>
-    import { userLocale } from '$lib/stores.js';
-    import { checkStringIncludes } from '$lib/utils/string_utils.js';
+    import MetaTags from "$lib/components/MetaTags.svelte";
+    import { checkStringIncludes } from "$lib/utils/string_utils.js";
+    import BoardHeader from "./BoardHeader.svelte";
+    import uniqBy from "lodash/uniqBy";
+    import { userLocale } from "$lib/stores";
+
     export let data;
-    let boards = data.adventureboards;
+
     let searchTerm = "";
+    let selectedMap = "";
+    let selectedBoardType = "";
 
     // TODO Cache the completed boards
 
-    $: if (searchTerm) {
-        boards = data.adventureboards.filter(board => board.panels.some(panel => checkStringIncludes(panel.quest.name.en_US, searchTerm) || checkStringIncludes(panel.quest.name.ja_JP, searchTerm)))
+    const allPanels = data.adventureboards.map((board) => board.panels).flat();
+    const allMaps = allPanels
+        .map((panel) => panel.quest.quest_achievement_condition.map_id)
+        .filter((map) => map);
+    const uniqueMaps = uniqBy(allMaps);
+
+    $: boards = data.adventureboards
+        .filter((board) => hasMatchingQuests(board, searchTerm))
+        .filter((board) => hasMatchingMap(board, selectedMap))
+        .filter((board) => hasMatchingBoardType(board, selectedBoardType));
+
+    function hasMatchingQuests(board, keyword) {
+        if (!keyword) return true;
+        return board.panels.some(
+            (panel) =>
+                checkStringIncludes(panel.quest.name.en_US, keyword) ||
+                checkStringIncludes(panel.quest.name.ja_JP, keyword)
+        );
+    }
+
+    function hasMatchingMap(board, mapId) {
+        if (!mapId) return true;
+        return board.panels.some(
+            (panel) => panel.quest.quest_achievement_condition.map_id === mapId
+        );
+    }
+
+    function hasMatchingBoardType(board, boardType) {
+        if (!boardType) return true;
     }
 </script>
 
-<input type="text" name="" id="" bind:value={searchTerm}>
-<p>{searchTerm}</p>
-<ul>
-    {#each boards as board}
-        <li>
-            {board.id}
-            <!-- <img class="board-img" src="/UI/Icon/Adventureboard/UI_Adventureboard_{board.icon_id}.png" alt="" width="208" height="128" loading="lazy"> -->
-            <strong>{board.name[$userLocale]}</strong>
-            {#each searchTerm ? board.panels.filter(panel => checkStringIncludes(panel.quest.name.en_US, searchTerm) || checkStringIncludes(panel.quest.name.ja_JP, searchTerm)) : board.panels as panel}
-                <p>{panel.quest.name[$userLocale]}</p>
+<MetaTags
+    title={`Adventure Boards — Bapharia`}
+    description={`Lookup all Adventure Boards in BLUE PROTOCOL. Find boards with duplicate tasks to save time completing them!`}
+/>
+
+<h1>Adventure Boards</h1>
+<p>
+    A tool to help players look up Adventure Boards. The same tasks can often be
+    found in multiple boards. Look up the tasks below to efficiently accept them
+    all in one go. <br /><br />
+    Learn more about Adventure Boards
+    <a href="/guides/adventure-boards">here</a>.
+</p>
+
+<div class="flex g-50 search-row">
+    <div class="grid" style="flex: 1; max-width: 700px; flex-basis: 200px">
+        <span class="component-label">Search</span>
+        <input
+            class="box"
+            type="text"
+            bind:value={searchTerm}
+            placeholder={`Search by board name or included quests, i.e. "Dragon's Rive" or "Goblin"`}
+        />
+    </div>
+
+    <div class="grid">
+        <span class="component-label">Board Type</span>
+        <select class="box" bind:value={selectedBoardType}>
+            <option value="">All</option>
+            <option value="1">Adventurer Rank</option>
+            <option value="1">Weapon</option>
+            <option value="1">Class Talisman</option>
+            <option value="1">Imagine</option>
+            <option value="1">Event</option>
+        </select>
+    </div>
+    <div class="grid">
+        <span class="component-label">Map</span>
+        <select class="box" bind:value={selectedMap}>
+            <option value="">All</option>
+            {#each uniqueMaps as map}
+                <option value={map}>{map}</option>
             {/each}
-        </li>
+        </select>
+    </div>
+</div>
+
+{#if searchTerm}
+    <p class="results-text">
+        {boards.length} results for "<b>{searchTerm}</b>"
+    </p>
+{:else}
+    <p class="results-text">{boards.length} results</p>
+{/if}
+
+<ul class="boards-list unstyled-list" role="list">
+    {#each boards as board}
+        {#if board.id !== 110010000}
+            <li class="box">
+                <BoardHeader {board} />
+                {#each board.panels.filter((panel) => checkStringIncludes(panel.quest.name.en_US, searchTerm) || checkStringIncludes(panel.quest.name.ja_JP, searchTerm)) as panel}
+                    <p style="color: var(--text2); font-size: var(--step-0)">
+                        {panel.quest.name[$userLocale]}
+                    </p>
+                {/each}
+            </li>
+        {/if}
     {/each}
 </ul>
 
 <style lang="scss">
-    .board-img {
-        background-image: url("/UI/AdventureBoard/UI_AdventureBoardBoardBg_copper.png");
+    .search-row {
+        flex-wrap: wrap;
+        row-gap: 1rem;
+    }
+
+    .results-text {
+        color: var(--text2);
+        font-size: var(--step-0);
+
+        b {
+            color: var(--text1);
+        }
+    }
+
+    .boards-list {
+        grid-template-columns: repeat(
+            auto-fill,
+            minmax(calc(208px + 2rem), 1fr)
+        );
+    }
+
+    @media (max-width: 1150px) {
+        h1 {
+            padding-top: 62px;
+        }
     }
 </style>
