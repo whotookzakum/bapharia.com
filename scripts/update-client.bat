6eb1774628120312261c5f5ecce283822fa6c678
Zakum <mirveo@gmail.com>
2024-05-23 19:01:37 -0700

    update api from 1.05.000 to 1.05.100; update very old client files

--- scripts/update-client.bat
+++ scripts/update-client.bat
@@ -1,7 +1,8 @@
 @echo off
 cd scripts\UnrealExporter
 UnrealExporter.exe config
+robocopy .\output\BLUEPROTOCOL\Content\UI ..\..\static\UI /E /MOV
+rm -rf .\output\BLUEPROTOCOL\Content\UI
 robocopy .\output\BLUEPROTOCOL ..\..\src\bp_client\japan /E /MOV 
-@REM xcopy .\output\BLUEPROTOCOL ..\..\src\bp_client\japan /s /y
 rm -rf .\output
 echo Finished exporting client to /src/bp_client/japan
