b4370e1201d59f4d6e3e2dcaa84b754e9df5a067
Zakum <mirveo@gmail.com>
2024-06-12 11:56:02 -0700

    add umodel to scripts

--- scripts/update-client.bat
+++ scripts/update-client.bat
@@ -1,4 +1,6 @@
 @echo off
+
+@REM UnrealExporter to export JSON, audio, etc. 
 cd scripts\UnrealExporter
 UnrealExporter.exe config
 robocopy .\output\BLUEPROTOCOL\Content\UI ..\..\static\UI /E /MOV
@@ -6,3 +8,7 @@ rm -rf .\output\BLUEPROTOCOL\Content\UI
 robocopy .\output\BLUEPROTOCOL ..\..\src\bp_client\japan /E /MOV 
 rm -rf .\output
 echo Finished exporting client to /src/bp_client/japan
+
+@REM UE Viewer to export 3d models
+cd ..
+umodel\umodel_materials -game="ue4.27" -path=C:\BandaiNamcoLauncherGames\BLUEPROTOCOL -aes=0xEA9051DDACE1CCF98A0510F0E370BD986A75C74756E0309E6A578A47AF564255 -pkg=\Game\Character\Weapon\WP\* -out=D:\bapharia.com\static -export -gltf
