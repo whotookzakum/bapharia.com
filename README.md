ec84f00423a4ae5d8fa2f6f443deeda859cabc58
Zakum <mirveo@gmail.com>
2024-08-23 05:03:01 -0700

    move kv script

--- README.md
+++ README.md
@@ -1,80 +1,10 @@
-1. `npm run upd` updates bp_api, bp_client, bpassets
-3. Pull submodule `bptranslatefiles` 
-4. Move `/old_routes/create` to `/routes/api/create` and visit `/api/create` to update the API; move back to `/old_routes` afterwards
+1. `npm run upd` updates `bp_api`, ``bp_client`, `bpassets`. Add support for auto-updating `bptranslatefiles`, for now simply pull the submodule.
+2. Visit `/api/create` to update KV.
+3. Replace the dev code with the prod code in `/api/[...path]/+server.js`
 
 In the `/api/create`, I delete SkillInfo from skills, as it's not needed in the final .json and takes a lot of data. This fixed running out of memory when building. May be needed for other jsons as well.
 
 If SSR and caching don't work, prerender each page locally and deploy via wrangler CLI.
-
-## Update checklist
-1. **Fetch and decrypt API**
-2. Update cloudflare KV
-3. Update worker schema
-
-1. **Datamine new client files**
-2. Update bp_client files
-3. Update UI files
-4. Create new map images
-5. Place new map markers
-
-## Deploy checklist
-- Change graphql endpoint `/src/client.js`
-- Move `kv` and `graphql` out of routes
-- Watch remote schema `/houdini.config.js`
-
-## ITEMS
-- Weapons
-    - 3d model
-    - stats (class, level, element, str, int...)
-    - recipe
-    - sale price
-- Imagine
-    - 3d model
-    - stats
-    - recipe
-    -sale price
-- Consumables
-    - effect
-    - sale price
-- Cosmetic Items
-    - 3d model
-- Materials (drops & resources)
-    - source (drops from x in y/found in y)
-- Plugs
-    - stats
-- Currencies
-- Stamps
-- Liquid Memory
-    - effect
-    - source
-
-## ENTITIES
-- Enemies
-    - 3d model
-    - stats
-    - location
-    - drops
-- NPCs
-    - location
-
-## OTHER
-- Achievements
-- Maps
-    - enemies
-    - resources
-    - NPCs
-- Buffs?
-- Skills
-    - class
-    - stats/effects
-- Classes?
-    - skills
-
-
-If possible have links show a preview of content on hover, i.e. hover a map name -> show map image; hover an item name -> show the header content with thumbnail and whatnot
-
-Search parameters i.e. `location:minster hills` or `drops:goblin staff` and show results that have those
-
 
 ## Architecture
 1. Detect user location & redirect to BNO or AGS version
---
+++ scripts/update-kv.js
@@ -0,0 +1,64 @@
+import fs from "fs"
+import { PRIVATE_CLOUDFLARE_API_KEY, PRIVATE_WORKERS_ACCOUNT_ID, PRIVATE_KV_NAMESPACE_ID_BNO_JA, PRIVATE_KV_NAMESPACE_ID_BNO_EN } from '$env/static/private';
+import { SUPPORTED_VERSIONS } from "$lib/constants"
+
+// Gather all API endpoints into objects with keys such as bno_ja, bno_en to efficiently bulk write to Cloudflare KV
+export async function consolidateDb() {
+    const files = import.meta.glob("../_api/*.js", { import: "default", eager: true })
+
+    const db = {}
+
+    for (const version of SUPPORTED_VERSIONS) {
+        const { publisher, locales } = version
+
+        for (const lang of locales) {
+            for (const [path, resolver] of Object.entries(files)) {
+                const fileName = path.split("/").pop().split(".")[0]
+                console.log(`${publisher}/${lang}:`, fileName)
+                let data = await resolver(lang)
+
+                if (fileName === "skills") {
+                    data = data.map(obj => {
+                        delete obj.SkillInfo
+                        return obj
+                    })
+                }
+
+                db[`${publisher}_${lang}`] ??= []
+                db[`${publisher}_${lang}`].push({ key: fileName, value: JSON.stringify(data) })
+
+                // Write to local JSON file
+                // if (!fs.existsSync(`./_api/json/${publisher}/${lang}`)) fs.mkdirSync(`./_api/json/${publisher}/${lang}`, { recursive: true });
+                // fs.writeFileSync(`./_api/json/${publisher}/${lang}/${fileName}.json`, JSON.stringify(data))
+            }
+        }
+    }
+    
+    return db
+}
+
+// Write to Cloudflare KV
+export function updateKv(db) {
+    Object.entries(db).forEach(([version, namespaces]) => {
+        let namespaceId;
+        switch (version) {
+            case "bno_en":
+                namespaceId = PRIVATE_KV_NAMESPACE_ID_BNO_EN
+                break;
+            case "bno_ja":
+                namespaceId = PRIVATE_KV_NAMESPACE_ID_BNO_JA
+                break;
+        }
+
+        console.log("Updating KV:", version)
+
+        fetch(`https://api.cloudflare.com/client/v4/accounts/${PRIVATE_WORKERS_ACCOUNT_ID}/storage/kv/namespaces/${namespaceId}/bulk`, {
+            method: 'PUT',
+            headers: { 'Content-Type': 'application/json', Authorization: `Bearer ${PRIVATE_CLOUDFLARE_API_KEY}` },
+            body: JSON.stringify(namespaces)
+        })
+            .then(response => response.json())
+            .then(response => console.log(response))
+            .catch(err => console.error(err));
+    })
+}
